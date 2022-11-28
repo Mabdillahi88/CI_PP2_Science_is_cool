c776812fd379135a022a80da9edaa382eceb7e5b
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-11-28 18:47:10 +0000

    completed basic style to main page and game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.options-text'));
 const progressText = document.querySelector('#gameText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#gameComplete');
