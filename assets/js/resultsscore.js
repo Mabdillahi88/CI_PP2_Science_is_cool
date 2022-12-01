868a97e3bf7d1711120b1ffa4515c28e84e4c065
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2022-12-01 15:47:49 +0000

    adding js script to results score page to save reresults

---
+++ assets/js/resultsscore.js
@@ -0,0 +1,37 @@
+const username = document.querySelector('#username')
+const saveResultsBtn = document.querySelector('#saveResultsBtn')
+const resultsScore = document.querySelector('#resultsScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = mostRecentScore
+
+username.addEventListener('keyup', () => {
+    saveResultsBtn.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+
+    
+}
